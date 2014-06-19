13a23913981476169a230196440603430aa8fbc1
michaelmargolis <memargolis@hotmail.com>
2014-06-19 11:02:53 +0100

    Updated test sketch to use I2C defines

--- TestIO/TestIO.ino
+++ TestIO/TestIO.ino
@@ -1,8 +1,17 @@
+#define USE_IMU
+#define USE_I2C  // Needed before asip.h if using I2C
 
 #include <asip.h>       // the base class definitions
 #include <asipIO.h>     // the core I/O class definition
-#include <OtherServices.h> // derived definitions for other services (distance and servo)
-#include <Servo.h> // needed for the servo service 
+#include <OtherServices.h> // derived definitions for other services (distance and servo)    
+#include <Servo.h>      // needed for the servo service 
+
+
+#include <Wire.h>     // needed for I2C
+#include <IMU.h>      // Inertial measurement services 
+#include "I2Cdev.h"   // needed for above  
+#include "MPU6050.h"
+#include "HMC5883L.h"
 
 // some defines for some test hardware
 #define SWITCH 13
@@ -25,6 +34,9 @@ const pinArray_t servoPins[] = {SERVO};
 
 asipServiceClass *services[] = { 
                                  &asipIO, // the core class for pin level I/O
+                                 &gyro3Axis,
+                                 &accelerometer3Axis,
+                                 &heading3Axis,
                                  &servos };
 
 int nbrServices = sizeof(services) / sizeof(asipServiceClass*);
@@ -39,6 +51,9 @@ void setup() {
   asip.registerPinMode(0,RESERVED_MODE);  // reserver pins used by the serial port 
   asip.registerPinMode(1,RESERVED_MODE);  
 
+  gyro3Axis.begin(NBR_GYRO_AXIS); // I2C services use begin method with just nbr of elements (axis)
+  accelerometer3Axis.begin(NBR_ACCEL_AXIS); // gyro and accel have x,y,z axis 
+  heading3Axis.begin(NBR_MAG_AXIS); // 3 raw values, 4th element is the calculated compass heading 
   servos.begin(1,1,servoPins);
   asip.sendPinModes(); // for debug
   asip.sendPinMap();
